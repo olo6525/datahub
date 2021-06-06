f03c404e6dc2bd35f851bd395337ddf80abc8ae2
olo6525 <oh___ya@naver.com>
2021-06-06 21:48:15 +0900

    832

--- storedb.py
+++ storedb.py
@@ -34,7 +34,7 @@ find50list_button=driver.find_element_by_xpath('//*[@id="contents"]/div[2]/div[3
 find50list_button.click()
 sleep(3)
 
-for i in range(735, 93655):
+for i in range(832, 93655):
     print(str(i-1))
     for j in range(1, 51):
         store_num =driver.find_element_by_xpath('//*[@id="tbl_bsn_list"]/tbody/tr['+str(j)+']/td[2]')
