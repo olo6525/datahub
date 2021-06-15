ab4c125a94b4382a306194ac004775e2bbacdbf0
olo6525 <oh___ya@naver.com>
2021-06-15 20:56:25 +0900

    infinit get data

--- storedb.py
+++ storedb.py
@@ -18,7 +18,7 @@ import requests
 db=pymysql.connect(user='user_me', passwd='Qordnjsrldustmq1!', host='3.36.86.210', db='storedb', charset='utf8')
 curs = db.cursor()
 
-driver = webdriver.Chrome(executable_path='chromedriver')
+driver = webdriver.Chrome(executable_path='./chromedriver')
 driver.implicitly_wait(time_to_wait=5)
 driver.get(url='http://www.foodsafetykorea.go.kr/portal/specialinfo/searchInfoCompany.do?menu_grp=MENU_NEW04&menu_no=2813#page1')
 driver.implicitly_wait(time_to_wait=1)
@@ -34,35 +34,49 @@ find50list_button=driver.find_element_by_xpath('//*[@id="contents"]/div[2]/div[3
 find50list_button.click()
 sleep(3)
 
-for i in range(832, 93655):
-    print(str(i-1))
-    for j in range(1, 51):
-        store_num =driver.find_element_by_xpath('//*[@id="tbl_bsn_list"]/tbody/tr['+str(j)+']/td[2]')
-        store_name = driver.find_element_by_xpath('//*[@id="tbl_bsn_list"]/tbody/tr['+str(j)+']/td[3]')
-        store_address = driver.find_element_by_xpath('//*[@id="tbl_bsn_list"]/tbody/tr['+str(j)+']/td[6]')
-        store_city = driver.find_element_by_xpath('//*[@id="tbl_bsn_list"]/tbody/tr['+str(j)+']/td[7]')
-        url = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode"
-        address = store_address.text
-        params = "query="+address+"&X-NCP-APIGW-API-KEY-ID=kvk4abf4qy&X-NCP-APIGW-API-KEY=24zN4WAXOvTx96sUImiKnYAFX2K8KRES8sYPEIC0"
-        res = requests.get(url,params)
-        location = json.loads(res.text)
-        latitude=float(0)
-        longitude=float(0)
+for i in range(6348, 93655):
+    driver.get(
+        url='http://www.foodsafetykorea.go.kr/portal/specialinfo/searchInfoCompany.do?menu_grp=MENU_NEW04&menu_no=2813#page' + str(
+            i))
+    print(str(i))
+    while True:
         try:
-            location = location['addresses'][0]
-            latitude = float(location['y'])
-            longitude = float(location['x'])
-            print(latitude)
-            print(longitude)
-        except Exception:
-            pass
-            print('주소없음')
-        sql="insert into storeinfo (storenum, storename, storeaddress, storelocation, latitude, longitude) values(%s, %s, %s, %s, %s, %s) on duplicate key update latitude=%s, longitude=%s"
-        val = (store_num.text, store_name.text, store_address.text, store_city.text, float(latitude), float(longitude), float(latitude), float(longitude))
-        curs.execute(sql,val)
-        db.commit()
-    driver.get(url='http://www.foodsafetykorea.go.kr/portal/specialinfo/searchInfoCompany.do?menu_grp=MENU_NEW04&menu_no=2813#page'+str(i))
-    sleep(10)
+            lodingicon = driver.find_element_by_xpath('//*[@id="fancybox-loading"]')
+            sleep(2)
+            print('wait')
+        except:
+            print('now get data')
+            for j in range(1, 51):
+                store_num = driver.find_element_by_xpath('//*[@id="tbl_bsn_list"]/tbody/tr[' + str(j) + ']/td[2]')
+                store_name = driver.find_element_by_xpath('//*[@id="tbl_bsn_list"]/tbody/tr[' + str(j) + ']/td[3]')
+                store_address = driver.find_element_by_xpath('//*[@id="tbl_bsn_list"]/tbody/tr[' + str(j) + ']/td[6]')
+                store_city = driver.find_element_by_xpath('//*[@id="tbl_bsn_list"]/tbody/tr[' + str(j) + ']/td[7]')
+                url = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode"
+                address = store_address.text
+                params = "query=" + address + "&X-NCP-APIGW-API-KEY-ID=kvk4abf4qy&X-NCP-APIGW-API-KEY=24zN4WAXOvTx96sUImiKnYAFX2K8KRES8sYPEIC0"
+                res = requests.get(url, params)
+                location = json.loads(res.text)
+                latitude = float(0)
+                longitude = float(0)
+                try:
+                    location = location['addresses'][0]
+                    latitude = float(location['y'])
+                    longitude = float(location['x'])
+                    print(latitude)
+                    print(longitude)
+                except Exception:
+                    pass
+                    print('주소없음')
+                sql = "insert into storeinfo (storenum, storename, storeaddress, storelocation, latitude, longitude) values(%s, %s, %s, %s, %s, %s) on duplicate key update latitude=%s, longitude=%s"
+                val = (
+                store_num.text, store_name.text, store_address.text, store_city.text, float(latitude), float(longitude),
+                float(latitude), float(longitude))
+                curs.execute(sql, val)
+                db.commit()
+            break
+
+
+
 
 
 
